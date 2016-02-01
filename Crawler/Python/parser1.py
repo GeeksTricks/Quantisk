306a85b004103a2eff2cff93b2cb1d1bbf15cecf
py6jlb <pyj6lb>
2016-02-01 20:09:49 +0300

    вынес скачивание сайтмапа в отдельный метод

--- Crawler/Python/parser1.py
+++ Crawler/Python/parser1.py
@@ -12,35 +12,37 @@ import xml.etree.ElementTree as ET
     - Отправить ссылки в БД
     - Написать функцию(класс) для скачивания страницы'''
 
-
 class GetUrlFromSitemap():
     def __init__(self, link):
         self.link = link
-    
     def get_sitemap_url(self, url): # Получаем ссылку на sitemap с использование reppy
         robots = RobotsCache()
         sitemapUrl = robots.sitemaps(url)[0]
         return sitemapUrl
-    
     def gen_ns(self, tag): # Функция для получения пространства имен xml, нужно для поиска по нему
         if tag.startswith('{'):
             ns, tag = tag.split('}')
             return ns[1:]
         else:
             return ''
-    
-    def parse_sitemap(self):
-        #скачиваем карту сайта(в случае неудачи выводим сообщение), затем получаем дерево xml
-        #дальше получаем корневой(родительский) элемент, и пространство имен для этого xml
-        #потом ищем нужный нам теш(loc) и вытаскиваем из него запись и печатаем ее в стандартный вывод
+    def sitemap_download(self):
+        #скачиваем карту сайта(в случае неудачи выводим сообщение)
         sitemapurl = self.get_sitemap_url(self.link)
-        sitemapxml = str(re.findall(r'.*\/(.+\..{3,4})$', sitemapurl)[0])
+        sitemap = str(re.findall(r'.*\/(.+\..{3,4})$', sitemapurl)[0])
         try:
-            with request.urlopen(sitemapurl) as response, open(sitemapxml, 'wb') as out_file:
+            with request.urlopen(sitemapurl) as response, open(sitemap, 'wb') as out_file:
                 shutil.copyfileobj(response, out_file)
+            return sitemap
         except error.HTTPError:
-            print('неполучилось скачать sitemap')
-        tree = ET.parse(sitemapxml)
+            print('Неполучилось скачать sitemap.')
+    def parse_sitemap(self):
+        #получаем дерево xml
+        #получаем корневой(родительский) элемент
+        #получаем пространство имен для этого xml
+        #потом ищем нужный нам тег('loc')
+        #вытаскиваем из него запись и печатаем ее в стандартный вывод или передаемкуда нибудь
+        sitemap = self.sitemap_download()
+        tree = ET.parse(sitemap)
         root = tree.getroot()
         namespaces = {'ns': self.gen_ns(root.tag)}
         for child in root:
@@ -50,7 +52,6 @@ class GetUrlFromSitemap():
 
 m = GetUrlFromSitemap('http://www.livejournal.ru')
 m.parse_sitemap
-
 '''
 
 
