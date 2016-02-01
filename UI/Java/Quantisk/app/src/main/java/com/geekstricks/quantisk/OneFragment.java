bae3627f39805695f130b8808e99aa6d38188ac9
kitayupov <k.i.tayupov@mail.ru>
2016-02-01 21:20:40 +0200

    init spinners and listview fullstats

--- UI/Java/Quantisk/app/src/main/java/com/geekstricks/quantisk/OneFragment.java
+++ UI/Java/Quantisk/app/src/main/java/com/geekstricks/quantisk/OneFragment.java
@@ -5,10 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Spinner;
 
-import com.geekstricks.quantisk.R;
+public class OneFragment extends Fragment {
 
-public class OneFragment extends Fragment{
+    Spinner personSpinner;
+    Spinner siteSpinner;
+    ListView resultsLV;
 
     public OneFragment() {
         // Required empty public constructor
@@ -23,7 +27,11 @@ public class OneFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        personSpinner = (Spinner) view.findViewById(R.id.spinner_person);
+        siteSpinner = (Spinner) view.findViewById(R.id.spinner_site);
+        resultsLV = (ListView) view.findViewById(R.id.results_lv);
+        return view;
     }
 
 }
