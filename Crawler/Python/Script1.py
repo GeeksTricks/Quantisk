9b994f09e5488ef18774ff99d32624ba07c0f5ab
unknown <VrublevskyAA@FKLW-309111.fk.corp>
2016-02-01 10:43:08 +0300

    Скачивание sitemap в script1

--- Crawler/Python/Script1.py
+++ Crawler/Python/Script1.py
@@ -2,19 +2,38 @@
 
 import re
 from urllib import request
+from urllib import error
+import shutil
 from reppy.cache import RobotsCache #нужно сначала установить reppy
 import xml.etree.ElementTree as ET
 
-site_url = 'http://' +'www.livejournal.com'
+site_url = 'http://' +'www.livejournal.com'#нужно получить из БД
 robots = RobotsCache()
-sitemap = robots.sitemaps(site_url)
+sitemapUrl = robots.sitemaps(site_url)
+
+def download_sitemap(link):
+	file_name = 'www.livejournal.com' + '_' + str(re.findall(r'.*\/(.+\..{3,4})$', link)[0])
+	try:
+		with request.urlopen(link) as response, open(file_name, 'wb') as out_file:
+			shutil.copyfileobj(response, out_file)
+	except error.HTTPError:
+		print('неполучилось скачать sitemap')
+	return file_name
+
+sitemapXml = download_sitemap(sitemapUrl[0])
+tree = ET.parse(sitemapXml)
+root = tree.getroot()
+
+
+
+
 
 
 '''
 TODO:
 
-- Скачать robots.txt
-- найти и скачать Sitemap
++ Скачать robots.txt
++ найти и скачать Sitemap
 - Вытащить из Sitemap все ссылки
 - Написать ORM класс дл я отправки ссылок в таблицу Pages
 - Отправить ссылки в БД
