b793362502c124d9647198a29bb5c81e146f40d9
py6jlb <pyj6lb>
2016-02-01 22:49:58 +0300

    Написал клас Загрузчика файлов. Каждая скачанная страница перезаписываеться при следующем вызове.

--- Crawler/Python/Downloader1.py
+++ Crawler/Python/Downloader1.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
-
-import re
 from urllib import request
 from urllib import error
 import shutil
-from reppy.cache import RobotsCache #нужно сначала установить reppy
-import xml.etree.ElementTree as ET
+import re
+
+# TODO: добавить логирование операций и возможных ошибок
+
+class Downloader():
+	def __init__(self):
+		self.filename = ''
+
+	def download(self, link):
+        page = str(re.findall(r'.*\/(.+\..{2,4})$', link)[0])
+		try:
+            with request.urlopen(link) as response,\
+                 open(page, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+            self.filename = page
+        except error.HTTPError:
+            print('Неполучилось скачать sitemap.')
+
 
 
 
