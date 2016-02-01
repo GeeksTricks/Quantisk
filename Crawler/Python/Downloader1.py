c0aba6b87389e1ed159e23ccad046940cfa3160b
unknown <VrublevskyAA@FKLW-309111.fk.corp>
2016-02-01 14:44:59 +0300

    переименовал файл затоговку под загрузчик

---
+++ Crawler/Python/Downloader1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import re
+from urllib import request
+from urllib import error
+import shutil
+from reppy.cache import RobotsCache #нужно сначала установить reppy
+import xml.etree.ElementTree as ET
+
+
+
+
+
+
