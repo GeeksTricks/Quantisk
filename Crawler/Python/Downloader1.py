cc2b34b458ab836ea6332df78a1a92943cdaa628
py6jlb <pyj6lb>
2016-02-02 23:29:16 +0300

    Разделил качалки Sitemap и HTMl по разным методам

--- Crawler/Python/Downloader1.py
+++ Crawler/Python/Downloader1.py
@@ -7,18 +7,31 @@ import re
 # TODO: добавить логирование операций и возможных ошибок
 
 class Downloader():
-	def __init__(self):
-		self.filename = ''
+    def __init__(self):
+        self.sitemap = ''
+        self.html_file = ''
 
-	def download(self, link):
-        page = str(re.findall(r'.*\/(.+\..{2,4})$', link)[0])
-		try:
+    def download_sitemap(self, link):
+        sitemap = str(re.findall(r'.*\/(.+\..{2,4})$', link)[0])
+        try:
             with request.urlopen(link) as response,\
-                 open(page, 'wb') as out_file:
+                 open(sitemap, 'wb') as out_file:
                 shutil.copyfileobj(response, out_file)
-            self.filename = page
+            self.sitemap = sitemap
         except error.HTTPError:
             print('Неполучилось скачать sitemap.')
+            
+    def download_html(self, id, link):
+        # ID мы должны получить из базы
+        # используем для имени файла
+        html_file = id + str(re.findall(r'.*\/.+(\..{2,4})$', link)[0])
+        try:
+            with request.urlopen(link) as response,\
+                 open(html_file, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+            self.html_file = html_file
+        except error.HTTPError:
+            print('Неполучилось скачать html.')
 
 
 
