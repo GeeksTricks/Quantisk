8b421c6977cde7805d3ec1ef6c0b40a5b465a5bc
TonyCross <starovoitov.anton@gmail.com>
2016-02-03 17:33:23 +0500

    Создал скрипт, который создает БД pages и кладет в нее данные

---
+++ Crawler/Python/sqlorm.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+import sqlite3
+
+engine = create_engine('sqlite:////media/mpak/Mainframe/python/Test/Pages.db', echo=True)
+
+
+metadata = MetaData()
+pagess_table = Table('pages', metadata,
+                    Column('id', Integer, primary_key=True),
+                    Column('page', String),
+                    )
+
+
+
+metadata.create_all(engine)
