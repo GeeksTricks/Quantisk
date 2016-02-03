ce7191657cc031c5c6b75baa27cb6c08637b0f04
TonyCross <starovoitov.anton@gmail.com>
2016-02-04 00:43:29 +0500

    Создал все классы, для БД ( Persons, Wordpairs, sites, pages, PersonsPageRank По умолчанию файл .db сохраняется в папке со скриптом и названием GeeksDB

--- Crawler/Python/sqlorm.py
+++ Crawler/Python/sqlorm.py
@@ -1,20 +1,70 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-import sqlite3
 
-engine = create_engine('sqlite:////media/mpak/Mainframe/python/Test/Pages.db', echo=True)
-
-
-metadata = MetaData()
-pagess_table = Table('pages', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('page', String),
-                    )
+engine = create_engine('sqlite:///./GeeksDB.db', echo=True)
+Base = declarative_base()
 
 
+class Persons(Base):
 
-metadata.create_all(engine)
+    __tablename__ = 'Persons'
 
-""" TODO
-1. Добавить ограничение VARCHAR
-2. Преобразовать в класс для каждой таблицы
+    id = Column(Integer, primary_key=True)
+    name = Column(String(2048))
+
+    def __repr__(self):
+        return "<Person '{}'>".format(self.name)
+
+
+class Wordpairs(Base):
+
+    __tablename__ = 'Wordpairs'
+
+    id = Column(Integer, primary_key=True)
+    keyword_1 = Column(String(2048))
+    keyword_2 = Column(String(2048))
+    distance = Column(Integer)
+    person_id = Persons.id
+
+    def __repr__(self):
+        pass
+
+class Sites(Base):
+
+    __tablename__ = 'Sites'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+
+    def __repr__(self):
+        pass
+
+class Pages(Base):
+
+    __tablename__ = 'Pages'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(2048))
+    site_id = Sites.id
+    found_date_time = Column(DateTime)
+    last_scan_date = Column(DateTime)
+
+    def __repr__(self):
+        pass
+
+
+
+class PersonsPageRank(Base):
+
+    __tablename__ = 'PersonsPageRank'
+
+    rank = Column(Integer, primary_key=True)
+    page_id = Pages.id
+    person_id = Persons.id
+
+    def __repr__(self):
+        pass
+
+
+Base.metadata.create_all(engine)
