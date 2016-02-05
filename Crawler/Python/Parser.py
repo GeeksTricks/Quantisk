cf58f795d17b78b16d5228f57901fbb1d5bbdaf4
py6jlb <pyj6lb>
2016-02-05 23:33:33 +0300

    TODO, и начал писать парсер Html

--- Crawler/Python/Parser.py
+++ Crawler/Python/Parser.py
@@ -6,22 +6,24 @@ from urllib import error
 import shutil
 from reppy.cache import RobotsCache #нужно сначала установить reppy
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+# нужна библиотека "pip install beautifulsoup4" и к ней еще нужна lxml, с их помощью парситься html;
+# TODO: а в идеале всетаки надо было использовать GRAB
+# по Роадмапу у нас есть цели асинхронного парсинга, так что пока надо плотно присамтриваться к этому делу
+# есть еще библиотека scrappy насколько я понял она тоже для разбора сайтов, тоже можно посмотреть.
+# с версии 3 надо будет использовать ее
 
 # TODO: добавить логирование операций и возможных ошибок
 
 class RobotsParser():
-    def __init__(self):
-        self.sitemap = ''
-
     def parse(self, site):
         # Функция которая получает ссылку на sitemap
         robots = RobotsCache()
-        self.sitemap = robots.sitemaps(site)
+        sitemaps = robots.sitemaps(site)
+        return sitemaps
 
 class SitemapParser():
-    def __init__(self):
-        self.urls = []
-
     def gen_ns(self, tag):
         # Функция для получения пространства имен xml, нужно для поиска по нему
         if tag.startswith('{'):
@@ -39,13 +41,11 @@ class SitemapParser():
         tree = ET.parse(sitemapfile)
         root = tree.getroot()
         namespaces = {'ns': self.gen_ns(root.tag)}
+        urls = []
         for child in root:
-            self.urls.append(child.find('ns:loc', namespaces=namespaces).text)
+            urls.append(child.find('ns:loc', namespaces=namespaces).text)
+        return urls
 
 class HtmlParser():
-    def __init__(self):
+    def parse_html(keyword1, keyword2, distance, html_file):
         pass
-        
-
-
-
