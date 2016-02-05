ccf19fc6759f77ee0e646bab8ac6297885cb4f3f
Allabergen <allabergen.su@gmail.com>
2016-02-05 19:37:33 +0600

    add settings that only admin can access

--- app/src/main/java/su/allabergen/quantisk/MainActivity.java
+++ app/src/main/java/su/allabergen/quantisk/MainActivity.java
@@ -241,6 +241,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         if (id == R.id.about) {
