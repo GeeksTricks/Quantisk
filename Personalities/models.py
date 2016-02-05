400a596a4cf24d1aa7ecf874914655bcf3de836d
Vitaliy <23neyron@gmail.com>
2016-02-06 01:10:34 +0300

    добавлена модель Личности

--- Personalities/models.py
+++ Personalities/models.py
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+	second_name = models.CharField(max_length=180, db_index=True, unique=True, blank=False, default='', verbose_name="Фамилия")
+		
+	class Meta:
+		verbose_name = 'Личность'
+		verbose_name_plural = 'Личности'
+
+	def __unicode__(self):
+
+		return '{}'.format(self.second_name)
