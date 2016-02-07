5380255c5d6f8f34db35f7849c03a58603bb5cfc
Allabergen <allabergen.su@gmail.com>
2016-02-07 18:11:13 +0600

    webservice demo (trying to work with webservice)

--- app/src/main/java/su/allabergen/quantisk/CommonStatActivity.java
+++ app/src/main/java/su/allabergen/quantisk/CommonStatActivity.java
@@ -3,14 +3,11 @@ package su.allabergen.quantisk;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -31,6 +28,9 @@ public class CommonStatActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String restUrl = "http://androidexample.com/media/webservice/JsonReturn.php";
+        new RestOperation(CommonStatActivity.this).execute(restUrl);
 
         Bundle extras = getIntent().getExtras();
         String name = extras.getString("NAME");
