83a3994b66e988937583c5aad2fd0d2e1c0d2eea
Allabergen <allabergen.su@gmail.com>
2016-02-19 01:45:59 +0600

    create VolleyGet and Put

--- app/src/main/java/su/allabergen/quantisk/VolleyGet.java
+++ app/src/main/java/su/allabergen/quantisk/VolleyGet.java
@@ -1,7 +1,117 @@
 package su.allabergen.quantisk;
 
+import android.content.Context;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Rabat on 19.02.2016.
  */
 public class VolleyGet {
+    RequestQueue requestQueue;
+    JSONObject jsonObject;
+    Context context;
+    String url;
+    String username;
+    String password;
+
+    public VolleyGet(Context context, String url, String username, String password) {
+        requestQueue = Volley.newRequestQueue(context);
+        jsonObject = new JSONObject();
+        this.context = context;
+        this.username = username;
+        this.password = password;
+        this.url = url;
+        getVolley();
+    }
+
+    public void getVolley() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject jsonPart = null;
+                            try {
+                                jsonPart = response.getJSONObject(i);
+                                Log.i("Quantisk id", String.valueOf(jsonPart.getInt("id")));
+                                Log.i("Quantisk id", jsonPart.getString("name"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        Toast.makeText(context, "Volley GET", Toast.LENGTH_SHORT).show();
+                        Log.i("Quantisk response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "Could not get content Volley GET", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("USERNAME", username);
+//                headers.put("PASSWORD", password);
+                String credentials = username + ":" + password;
+                String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json, charset=UTF-8");
+                headers.put("Authorization", auth);
+                Log.i("Quantisk header", headers.toString());
+                return headers;
+            }
+        };
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Toast.makeText(context, "Volley GET", Toast.LENGTH_SHORT).show();
+                        Log.i("Quantisk response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "Could not get content Volley GET", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("USERNAME", username);
+//                headers.put("PASSWORD", password);
+                String credentials = username + ":" + password;
+                String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json, charset=UTF-8");
+                headers.put("Authorization", auth);
+                Log.i("Quantisk header", headers.toString());
+                return headers;
+            }
+        };
+        requestQueue.add(jsonArrayRequest);
+    }
 }
--- app/src/main/java/su/allabergen/quantisk/VolleyPut.java
+++ app/src/main/java/su/allabergen/quantisk/VolleyPut.java
@@ -1,7 +1,82 @@
 package su.allabergen.quantisk;
 
+import android.content.Context;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Rabat on 19.02.2016.
  */
 public class VolleyPut {
+    RequestQueue requestQueue;
+    JSONObject jsonObject;
+    Context context;
+    String url;
+    String username;
+    String password;
+    int id;
+
+    public VolleyPut(Context context, String url, int id, String username, String password) {
+        requestQueue = Volley.newRequestQueue(context);
+        jsonObject = new JSONObject();
+        this.context = context;
+        this.username = username;
+        this.password = password;
+        this.url = url;
+        this.id = id + 1;
+        getVolley();
+    }
+
+    public void getVolley() {
+        try {
+            jsonObject.put("id", id);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, url, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(context, "Deleted Person with ID = " + id, Toast.LENGTH_SHORT).show();
+                        Log.i("Quantisk header", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "Could not delete Person with ID = " + id, Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("USERNAME", username);
+//                headers.put("PASSWORD", password);
+                String credentials = username + ":" + password;
+                String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json, charset=UTF-8");
+                headers.put("Authorization", auth);
+                Log.i("Quantisk header", headers.toString());
+                return headers;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
+    }
 }
