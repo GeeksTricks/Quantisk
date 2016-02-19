c2d808e44b71a8dce6022eec7cc6b1077ced9912
Allabergen <allabergen.su@gmail.com>
2016-02-20 01:57:35 +0600

    Retrofit Demo

--- app/src/main/java/su/allabergen/quantisk/RetrofitAPI.java
+++ app/src/main/java/su/allabergen/quantisk/RetrofitAPI.java
@@ -1,7 +1,16 @@
 package su.allabergen.quantisk;
 
+import org.json.JSONArray;
+
+import retrofit2.Callback;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
 /**
  * Created by Rabat on 19.02.2016.
  */
 public interface RetrofitAPI {
+    @GET("/v1/persons/")
+    @Headers({"Accept-Encoding: application/json", "user1:qwerty1"})
+    void getFeed(Callback<JSONArray> jsonArray);
 }
