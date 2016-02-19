142544b8ac35caa64aed9b742cc75ff856594dd5
sailenke <sailenke@gmail.com>
2016-02-19 23:31:05 +0300

    Настройки БД изменены для openshift

--- function.php
+++ function.php
@@ -3,16 +3,17 @@ function getDbConnect() {
 	static $link;
 
 	// настройки подключения к бд
-	$hostname = 'localhost';
-	$username = 'root';
-	$password = '';
+	$hostname = '56c6b4a92d527192b800006a-geekstriks.rhcloud.com:37321';
+	$username = 'apiuser';
+	$password = 'api_probation2016';
 
 	// только одно соединение с бд
 	if ($link === null) {
 		$link = mysqli_connect($hostname, $username, $password); // подключаемся к БД
 		mysqli_query($link, 'SET NAMES utf8');
 		mysqli_set_charset($link, 'utf8');
-		$db_selected = mysqli_select_db($link, 'probation'); //выбор базы
+		$db_selected = mysqli_select_db($link, 'db'); //выбор базы
+
 	return $link;
 	}
 }
