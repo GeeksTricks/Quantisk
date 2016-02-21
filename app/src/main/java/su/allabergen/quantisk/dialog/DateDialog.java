f43c0f2adb9280411f7b1166480633e619d00c52
Allabergen <allabergen.su@gmail.com>
2016-02-21 21:42:11 +0600

    modify getting date

--- app/src/main/java/su/allabergen/quantisk/dialog/DateDialog.java
+++ app/src/main/java/su/allabergen/quantisk/dialog/DateDialog.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -26,13 +27,20 @@ public class DateDialog extends DialogFragment implements DatePickerDialog.OnDat
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
+
         int day = c.get(Calendar.DAY_OF_MONTH);
+
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-        String date = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
-        dateToPick.setText(date);
+        String month0 = (((monthOfYear < 10) ? "0" : "") + (monthOfYear + 1));
+        String day0 = (((dayOfMonth < 10) ? "0" : "") + dayOfMonth);
+
+//        String date = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+        String date2 = year + "-" + month0 + "-" + day0;
+        Log.i("DATE 2", year + "-" + month0 + "-" + day0);
+        dateToPick.setText(date2);
     }
 }
