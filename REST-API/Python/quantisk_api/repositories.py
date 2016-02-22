3e2e3311353080f34a1a6ccc4efd18b923a9b28b
Glafira Ivanova <ivanova.glafira@yandex.ru>
2016-02-22 03:27:06 +0300

    Убраны методы для определения создателя.

--- REST-API/Python/quantisk_api/repositories.py
+++ REST-API/Python/quantisk_api/repositories.py
@@ -4,13 +4,13 @@ from collections import namedtuple
 from werkzeug.security import generate_password_hash
 
 
-Person = namedtuple('Person', ['id', 'name', 'user_id'])
-WordPair = namedtuple('WordPair', ['id', 'keyword1', 'keyword2', 'distance', 'person_id', 'user_id'])
-Site = namedtuple('Site', ['id', 'name', 'user_id'])
+Person = namedtuple('Person', ['id', 'name'])
+WordPair = namedtuple('WordPair', ['id', 'keyword1', 'keyword2', 'distance', 'person_id'])
+Site = namedtuple('Site', ['id', 'name'])
 Page = namedtuple('Page', ['id', 'url', 'site_id', 'found_date_time', 'last_scan_date'])
 TotalRank = namedtuple('Rank', ['rank', 'site_id', 'person_id'])
 DailyRank = namedtuple('DailyRank', ['rank', 'day', 'site_id', 'person_id'])
-User = namedtuple('User', ['id', 'login', 'pass_hash', 'role'])
+User = namedtuple('User', ['id', 'login'])
 
 
 class NonUniqueError(Exception):
@@ -28,63 +28,52 @@ class UserRepo(Repo):
         u = UserModel.query.get(id)
         if u is None:
             return None
-        return User(u.id, u.login, u.pass_hash, u.role)
+        return User(u.id, u.login)
 
     def get_by_login(self, login):
         query = UserModel.query
         u = query.filter_by(login=login).first()
         if u is None:
             return None
-        return User(u.id, u.login, u.pass_hash, u.role)
-
-    def get_user_role(self, id):
-        pass
+        return User(u.id, u.login)
 
     def get_all(self):
         u_list = UserModel.query.all()
         if u_list is None:
             return None
-        return [User(u.id, u.login, u.pass_hash, u.role) for u in u_list]
+        return [User(u.id, u.login) for u in u_list]
 
-    def add(self, login, password, role):
+    def add(self, login, password):
         if self.get_by_login(login):
             raise NonUniqueError('This login is already taken')
         p_hash = generate_password_hash(password)
-        u = UserModel(login, p_hash, role)
+        u = UserModel(login, p_hash)
         db.session.add(u)
         db.session.commit()
-        return User(u.id, u.login, u.pass_hash, u.role)
+        return User(u.id, u.login)
 
     def delete(self, id):
         u = UserModel.query.get(id)
         if u is None:
             return None
-
         db.session.delete(u)
         db.session.commit()
-        return User(u.id, u.name)
+        return User(u.id, u.login)
 
 
 class PersonRepo(Repo):
-
-    def get_owner_id(self, id):
-        p = UserModel.query.get(id)
-        user_id = p.user_id
-        if p is None or user_id is None:
-            return None
-        return user_id
 
     def get_by_id(self, id):
         p = PersonModel.query.get(id)
         if p is None:
             return None
-        return Person(p.id, p.name, p.user_id)
+        return Person(p.id, p.name)
 
     def get_all(self):
         p_list = PersonModel.query.all()
         if p_list is None:
             return None
-        return [Person(p.id, p.name, p.user_id) for p in p_list]
+        return [Person(p.id, p.name) for p in p_list]
 
     def set(self, id, name):
         p = PersonModel.query.get(id)
@@ -92,13 +81,13 @@ class PersonRepo(Repo):
             return None
         p.name = name
         db.session.commit()
-        return Person(p.id, p.name, p.user_id)
+        return Person(p.id, p.name)
 
-    def add(self, name, user_id):
-        p = PersonModel(name, user_id)
+    def add(self, name):
+        p = PersonModel(name)
         db.session.add(p)
         db.session.commit()
-        return Person(p.id, p.name, p.user_id)
+        return Person(p.id, p.name)
 
     def delete(self, id):
         p = PersonModel.query.get(id)
@@ -106,21 +95,16 @@ class PersonRepo(Repo):
             return None
         db.session.delete(p)
         db.session.commit()
-        return Person(p.id, p.name, p.user_id)
+        return Person(p.id, p.name)
 
 
 class WordPairRepo(Repo):
 
-    def get_owner_id(self, id):
-        user_id = db.session.query(PersonModel.user_id).join(WordpairModel).filter_by(id=id).scalar()
-        return user_id
-
     def get_by_id(self, id):
         wp = WordpairModel.query.get(id)
-        user_id = self.get_owner_id(id)
-        if wp is None or user_id is None:
+        if wp is None:
             return None
-        return WordPair(wp.id, wp.keyword1, wp.keyword2, wp.distance, wp.person_id, user_id)
+        return WordPair(wp.id, wp.keyword1, wp.keyword2, wp.distance, wp.person_id)
 
     def get_by_person_id(self, person_id):
         query = WordpairModel.query
@@ -128,18 +112,13 @@ class WordPairRepo(Repo):
         if query is None:
             return None
         wp_list = query.all()
-        return [WordPair(wp.id, wp.keyword1, wp.keyword2, wp.distance, wp.person_id, self.get_owner_id(wp.id)) for wp in wp_list]
+        return [WordPair(wp.id, wp.keyword1, wp.keyword2, wp.distance, wp.person_id) for wp in wp_list]
 
     def get_all(self):
         wp_list = WordpairModel.query.all()
         if wp_list is None:
             return None
-        return [WordPair(wp.id,
-                         wp.keyword1,
-                         wp.keyword2,
-                         wp.distance,
-                         wp.person_id,
-                         self.get_owner_id(wp.id)) for wp in wp_list]
+        return [WordPair(wp.id, wp.keyword1, wp.keyword2, wp.distance, wp.person_id) for wp in wp_list]
 
     def add(self, keyword1, keyword2, distance, person_id):
         wp = WordpairModel(keyword1, keyword2, distance, person_id)
@@ -173,13 +152,13 @@ class SiteRepo(Repo):
         s = SiteModel.query.get(id)
         if s is None:
             return None
-        return Site(s.id, s.name, s.user_id)
+        return Site(s.id, s.name)
 
     def get_all(self):
         s_list = SiteModel.query.all()
         if s_list is None:
             return None
-        return [Site(s.id, s.name, s.user_id) for s in s_list]
+        return [Site(s.id, s.name) for s in s_list]
 
     def set(self, id, name):
         s = SiteModel.query.get(id)
@@ -187,13 +166,13 @@ class SiteRepo(Repo):
             return None
         s.name = name
         db.session.commit()
-        return Site(s.id, s.name, s.user_id)
+        return Site(s.id, s.name)
 
-    def add(self, name, user_id):
-        s = SiteModel(name, user_id)
+    def add(self, name):
+        s = SiteModel(name)
         db.session.add(s)
         db.session.commit()
-        return Site(s.id, s.name, s.user_id)
+        return Site(s.id, s.name)
 
     def delete(self, id):
         s = SiteModel.query.get(id)
@@ -201,7 +180,7 @@ class SiteRepo(Repo):
             return None
         db.session.delete(s)
         db.session.commit()
-        return Site(s.id, s.name, s.user_id)
+        return Site(s.id, s.name)
 
 
 class RankRepo(Repo):
