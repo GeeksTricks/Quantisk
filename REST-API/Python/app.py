a0feb9689e57bba840cc443c7233fa070d63482b
Glafira Ivanova <ivanova.glafira@yandex.ru>
2016-02-22 03:49:28 +0300

    app.py перемещен из корня, чтобы избежать конфликтов с краулером.

--- REST-API/Python/app.py
+++ REST-API/Python/app.py
@@ -2,11 +2,10 @@ import sys
 import os
 sys.path.extend([os.path.dirname(__file__)])
 from quantisk_api import app as application
-import os 
 
 
 ip = os.environ.get('OPENSHIFT_PYTHON_IP', '0.0.0.0') 
 port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 5000)) 
 
 if __name__ == '__main__': 
-    application.run(debug=False, host=ip, port=port)
+    application.run(debug=True, host=ip, port=port)
