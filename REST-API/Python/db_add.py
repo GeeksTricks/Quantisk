c292aedef499f4b52a96fb62e04631a42d53fb7e
Glafira Ivanova <ivanova.glafira@yandex.ru>
2016-02-22 03:51:05 +0300

    Убрано все, что касалось ролей. Создается только один юзер.

--- REST-API/Python/db_add.py
+++ REST-API/Python/db_add.py
@@ -2,22 +2,17 @@ from quantisk_api.repositories import rank_repo, page_repo, site_repo, wordpair_
 import dateutil.parser as dp
 
 
-for i in range(1, 6):
-    user_repo.add('user' + str(i) + '@example.com', 'qwerty' + str(i), 1)
-
-for i in range(6, 11):
-    user_repo.add('user' + str(i) + '@example.com', 'qwerty' + str(i), 2)
+user_repo.add('admin@example.com', 'qwerty')
 
 for i in range(1, 11):
-    person = person_repo.add('персона' + str(i), i)
+    person = person_repo.add('персона' + str(i))
     for j in range(10):
         wordpair_repo.add('word' + str(j), 'fff' + str(j), 2, person.id)
 
-site = site_repo.add('lenta.ru', 1)
-site1 = site_repo.add('lenta.ru', 2)
+site = site_repo.add('livejournal.com')
 
-for i in range(1,11):
-    for j in range(1,11):
+for i in range(1, 11):
+    for j in range(1, 11):
         page_repo.add(
             '/posts/' + str(i) + str(j),
             site.id,
@@ -25,14 +20,11 @@ for i in range(1,11):
             dp.parse('2016-02-{0:02d}T{1:02d}:55:33'.format(i, j)),
         )
 
-for i in range(1,51):
+for i in range(1, 51):
     rank_repo.add_rank(1, page_id=i, person_id=1)
-for i in range(51,101):
+for i in range(51, 101):
     rank_repo.add_rank(1, page_id=i, person_id=2)
-for i in range(1,101):
+for i in range(1, 101):
     rank_repo.add_rank(1, page_id=i, person_id=3)
-for i in range(30,70):
+for i in range(30, 70):
     rank_repo.add_rank(2, page_id=i, person_id=4)
-
-
-
