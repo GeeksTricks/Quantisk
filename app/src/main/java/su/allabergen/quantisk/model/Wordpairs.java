6a6c207d2d2ef718ee3690658e96069b402562c9
Allabergen <allabergen.su@gmail.com>
2016-02-25 06:02:41 +0600

    do refactoring

--- app/src/main/java/su/allabergen/quantisk/model/Wordpairs.java
+++ app/src/main/java/su/allabergen/quantisk/model/Wordpairs.java
@@ -4,8 +4,8 @@ package su.allabergen.quantisk.model;
  * Created by Rabat on 20.02.2016.
  */
 public class Wordpairs {
-    private int id;
     private int distance;
+    private int id;
     private int person_id;
     private String keyword1;
     private String keyword2;
