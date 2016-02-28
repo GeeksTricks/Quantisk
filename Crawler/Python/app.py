7857b026413883b0dd4ae2f703326e16583f07c0
Vrublevsky Aleksey <py6jlb@gmail.com>
2016-02-28 12:58:34 +0300

    Парсер который работает в облаке.

--- Crawler/Python/app.py
+++ Crawler/Python/app.py
@@ -17,13 +17,27 @@ wordpairs = WordpairsRepo()
 base_repo = Repo()
 
 
+def get_url_from_file(file):
+    for line in file:
+        row = line.split('+!+')
+        yield {'url': row[0], 'site': row[1]}
+
+
 def main():
     os.chdir(r'/tmp')
     while True:
         site_tuple = sites.get_all()
+        # создаем буферный файл(для экономии памяти)
+        tmp_file = open('urls_tmp_file.txt', 'w')
         for site in site_tuple:
-            url_list = url_handler.get_url_from_sitemap(site.name)
-            pages.add_url(url_list, site.id)
+            print(site.name)
+            url_handler.get_url_from_sitemap(site.name, tmp_file, site.id)
+        tmp_file.close()
+
+        tmp_file = open('urls_tmp_file.txt', 'r')
+
+        for url in get_url_from_file(tmp_file):
+            pages.add_url(url['url'], url['site'])
 
         url_tuple_list = pages.get_not_scan_urls()
         query_tuple_list = wordpairs.get_query()
@@ -31,13 +45,12 @@ def main():
         for url_tuple in url_tuple_list:
             page_id = url_tuple.id
             file = download.download_file(url_tuple.url)
-            print(url_tuple.url)
             if file:
                 pages.add_last_scan_date(page_id)
                 page_parser.parse_html(file, query_tuple_list, url_tuple.id)
                 os.remove(file)
             else:
-                print('нескачалось(((')
+                print('нескачалось((( ' + url_tuple.url)
                 pass
 
         time.sleep(43200)
